492ad6f51323dc26b8a80ae2162829ad454395e6
Seongbin Kim <seongbin9786@gmail.com>
2025-01-13 15:39:04 +0900

    feat: 렌더링 결과 확인을 위해 최초 마운트 이후 addTodo dispatch 수행

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./App.css";
+import { TodoActionDispatchers } from "./data/TodoActionDispatchers.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
+TodoActionDispatchers.addTodo("Hello, world 1");
+TodoActionDispatchers.addTodo("Hello, world 2");
+TodoActionDispatchers.addTodo("Hello, world 3");
